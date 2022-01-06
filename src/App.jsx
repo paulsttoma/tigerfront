92b096964d9db2f96883ecd782baf127b043e7c5
Pauls Toma <94072281+paulsttoma@users.noreply.github.com>
2022-01-06 01:38:13 -0500

    new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from '@material-ui/core/Button'
+
+
+function App() {
+  return (
+    <div >
+        <Button variant = "contained"> Mint </Button>
+    </div>
+  );
+}
+
+export default App;
