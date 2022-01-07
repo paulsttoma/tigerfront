4c27cf457c62bec034cfb0b61212aba23042e293
Pauls Toma <94072281+paulsttoma@users.noreply.github.com>
2022-01-06 23:09:05 -0500

    componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import * as React from "react";
 import "./App.css";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import AppBar from "@material-ui/core/AppBar";
-
+import TourCard from "./components/TourCard";
+//import Button from "@material-ui/core/Button";
+//import Typography from "@material-ui/core/Typography";
+//import AppBar from "@material-ui/core/AppBar";
 
 function App() {
   return (
-    <div className="App">
-        <Button variant="contained" color="secondary" size="large">
-          Hello World
-        </Button>
+    <div>
+     <TourCard />
     </div>
   );
 }
---
+++ src/components/TourCard.jsx
@@ -0,0 +1,5 @@
+const TourCard = () => {
+  return <div>See me</div>;
+};
+
+export default TourCard;
