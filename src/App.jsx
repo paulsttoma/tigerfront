74ede895167a29a8cf1152e7e8a4e9d0b374ba90
Pauls Toma <94072281+paulsttoma@users.noreply.github.com>
2022-01-06 22:17:54 -0500

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import * as React from "react";
+import "./App.css";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Button variant="contained" color="secondary">
+        <Button variant="contained" color="secondary" size="large">
           Hello World
         </Button>
-      </header>
     </div>
   );
 }
